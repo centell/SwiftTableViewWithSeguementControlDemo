6e493a4cf8c37fc8c1efdd9c5ad7f0697979e480
Centell <centell@Centellui-MacBook-Pro.local>
2017-08-23 12:10:16 +0900

    Initial Commit

---
+++ TableViewWithSeguementControlDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewWithSeguementControlDemo
+//
+//  Created by Centell on 2017. 8. 23..
+//  Copyright © 2017년 Centell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
